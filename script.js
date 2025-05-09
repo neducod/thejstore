66ad9f3b86b68bdafd32f44a87b72271ef36a8b2
neducod <nedunony16@gmail.com>
2025-05-09 17:22:38 +0100

    Currently Debugging

--- script.js
+++ script.js
@@ -53,3 +53,14 @@ hamburger.addEventListener('click', ()=> {
       });
     });
   });
+
+
+
+
+
+  function addToCart(productName) {
+    let cart = JSON.parse(localStorage.getItem('cart')) || [];
+    cart.push(productName);
+    localStorage.setItem('cart', JSON.stringify(cart));
+    alert(productName + " added to cart!");
+  }
